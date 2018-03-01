cb6168410da81548bd6bf1efe35cd0c45ccf35a7
Emma Stout <emma.stout01@gmail.com>
2018-03-01 12:36:02 -0600

    added two photos and event listeners

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import panda from './panda.jpg';
+import goose from './goose.jpg';
+
 
 // This is a 'smart' component. 
 
 const myName = 'Emma'; 
+
+// Note: Here's an example of using variables to set attributes. It cleans up the render() code below.
+const sideLength = "400px";
+
+const pandaPic = (
+  <img 
+    src= {panda}
+    alt="panda" 
+    height={sideLength} 
+    width={sideLength} />
+);
+
+const goosePic = (
+  <img 
+    src= {goose}
+    alt="goose" 
+    height={sideLength} 
+    width={sideLength} />
+);
+
+// This is called when the panda pic is clicked
+const myPandaFunc = () => {
+  console.log('hey there panda bear!')
+}
+
+// This is called when the goose pic is clicked
+const myGooseFunc = () => {
+  console.log('you silly goose!')
+}
 
 class Header extends Component {
     render() {
@@ -17,6 +49,9 @@ class Header extends Component {
           </header>
           {/* Note: Everything needs a closing tag in JSX. <br> won't fly; it needs to be <br />. */}
           <br />
+          {/* Below: using an event listener to call a function on click of the div */}
+          <div onClick={myPandaFunc}>{pandaPic}</div>
+          <div onClick={myGooseFunc}>{goosePic}</div>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
