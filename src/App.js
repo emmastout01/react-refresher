fcf88fe1f94c40d8b1883d81c7c313ec64785c7a
Emma Stout <emma.stout01@gmail.com>
2018-03-01 10:44:00 -0600

    moved header to component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// Note: Imports should always go first before adding any global variables
+import React, { Component } from 'react';
+// Destructuring: We're destructuring component. This is a shorthand way of saying 'import that one thing Component from React
+// Const Component = React.Component
+// This is not a React thing, it's an ES6 thing, but it's used all the time in React
+import Header from './components/Header/Header.js'
+  // Note: 'Header' does NOT need to match what we're exporting from the header.js file. Here, we're just naming a variable that we'll use
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
