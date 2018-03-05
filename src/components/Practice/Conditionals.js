b74979dfacf1a4328c8fae6076501cdb2cb8b303
Emma Stout <emma.stout01@gmail.com>
2018-03-05 09:28:01 -0600

    added conditional example

--- src/components/Practice/Conditionals.js
+++ src/components/Practice/Conditionals.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+let isACat = true; 
+
 class Conditionals extends Component {
   render() {
     return (
       <div >
         <h3>Conditionals Practice</h3>
         <p>Hello world! Test Test Test!</p>
+        {/* Below: This is a conditional format that is common in React. It's just vanilla JavaScript. What a handy way to write simple conditionals! */}
+        {/* The format: condition ? if true do this thing : if false do this thing */}
+        <h4>{isACat = true ? 'You\'re a cool cat!' : 'What\'s up dog?'}</h4>
       </div>
     );
   }
